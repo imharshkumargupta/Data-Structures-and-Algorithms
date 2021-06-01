34c7c400d1968320175f857625edea6ac607241e
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2021-06-01 11:34:45 +0530

    Implement array from scratch

---
+++ arrayFromScratch.js
@@ -0,0 +1,47 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    let lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  insert(item, index) {
+    for (let i = this.length; i >= index; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[index] = item;
+    this.length++;
+    return this.data;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("you");
+newArray.push("!");
+newArray.insert("hello there", 1);
+console.log(newArray);
